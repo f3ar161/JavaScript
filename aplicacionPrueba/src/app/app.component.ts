e7936df1e419699bab590ae9eca7b44188319923
aaaaa <aaaaa>
2017-02-23 17:45:03 -0500

    update

--- aplicacionPrueba/src/app/app.component.ts
+++ aplicacionPrueba/src/app/app.component.ts
@@ -1,10 +1,30 @@
-import { Component } from '@angular/core';
-
+import {Component, OnInit} from "@angular/core";
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'app works!';
+export class AppComponent implements OnInit{
+  title:string ="Hola amigos";
+  nombre:String="";
+  apellido:String="";
+
+  constructor() {
+  this.nombre="Angel";
+  this.apellido="Molina";
+  console.log("Inicio el constructor");
+
+  }
+
+  ngOnInit(){
+    this.nombre="Enrique";
+    this.apellido="Ortiz";
+    console.log("On Init");
+  }
+  nombreCompleto():string{
+    return `${this.nombre} ${this.apellido}`
+  }
+  hizoClick(){
+    console.log("hizo click");
+  }
 }
