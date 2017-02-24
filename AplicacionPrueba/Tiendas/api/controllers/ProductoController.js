ec23c253d9bd8d686279ea825eaacc1cdd7a764d
aaaaa <aaaaa>
2017-02-24 16:23:12 -0500

    update

---
+++ AplicacionPrueba/Tiendas/api/controllers/ProductoController.js
@@ -0,0 +1,15 @@
+/**
+ * ProductoController
+ *
+ * @description :: Server-side logic for managing Productoes
+ * @help        :: See http://sailsjs.org/#!/documentation/concepts/Controllers
+ */
+
+module.exports = {
+	  attributes: {
+	    idTienda:{
+	      model:"Tienda"
+      }
+    }
+};
+
